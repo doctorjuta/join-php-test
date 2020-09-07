749f4cf2427e909cc0a611aafe3fcbd6466701b9
Andrii Dutchak <opora.foto@gmail.com>
2020-09-07 14:24:18 +0300

    Added base user authentication. Added form for candidates.

---
+++ resources/views/candidates/form.blade.php
@@ -0,0 +1,37 @@
+<form>
+    <div class="form-group">
+        <label for="cand-email">Your E-mail</label>
+        <input type="email" class="form-control" id="cand-email" name="cand-email">
+    </div>
+    <div class="form-group">
+        <label for="cand-passwd">Set a Password</label>
+        <input type="password" class="form-control" id="cand-passwd" name="cand-passwd">
+    </div>
+    <div class="row">
+        <div class="col-sm">
+            <div class="form-group">
+                <label for="cand-fname">First Name</label>
+                <input type="text" class="form-control" id="cand-fname" name="cand-fname">
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="form-group">
+                <label for="cand-lname">Last Name</label>
+                <input type="text" class="form-control" id="cand-lname" name="cand-lname">
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="cand-tel">Phone number</label>
+        <input type="tel" class="form-control" id="cand-tel" name="cand-tel">
+    </div>
+    <div class="form-group">
+        <label for="cand-photo">Photo</label>
+        <input type="file" class="form-control-file" id="cand-photo" name="cand-photo">
+    </div>
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="cand-agree" name="cand-agree">
+        <label class="form-check-label" for="cand-agree">I agree to JOIN's <a href="#">terms and conditions</a> as well as the <a href="#">privacy policy</a>.</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Apply now for this job</button>
+</form>
