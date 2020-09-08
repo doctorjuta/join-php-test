529ac361eb01b471c0e15454f98e418e0dc7c7aa
Andrii Dutchak <opora.foto@gmail.com>
2020-09-08 14:27:02 +0300

    Fix for HTTPS setter.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('FORCE_HTTPS',false)) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
